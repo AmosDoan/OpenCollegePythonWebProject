b66eaf89eb281d569e6541e9300bb8eda1386f7c
amos <amos.doan@gmail.com>
2019-11-13 19:46:37 +0900

    Session #0 연습

---
+++ privateClass/stringPractice.py
@@ -0,0 +1,13 @@
+a = "2 3"
+b = a.split()
+print(b[0])
+print(b[1])
+result = int(b[0]) + int(b[1])
+print(result)
+
+a = "2X3"
+b = a.split('X')
+print(b[0])
+print(b[1])
+result = int(b[0]) * int(b[1])
+print(result)
---
+++ privateClass/variable.py
@@ -0,0 +1,34 @@
+a = 2
+b = 3
+print(a)
+print(b)
+# 덧셈
+c = a + b
+print(c)
+# 곱셈
+d = a * b
+print(d)
+# 나눗셈
+e = a / b
+print(e)
+# 나눗셈 하고 몫
+f = a // b
+print(f)
+# 나눗셈 하고 나머지
+g = a % b
+print(g)
+# 정수형으로 변환
+stringVal = "1"
+integerVal = int(stringVal)
+print(integerVal)
+print(type(stringVal))
+print(type(integerVal))
+
+# 문자열 변수
+a = "hello"
+b = "world"
+c = a + b
+d = 3
+d = str(d)
+e = c + d
+print(e)
