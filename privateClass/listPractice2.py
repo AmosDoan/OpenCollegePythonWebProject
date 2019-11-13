8e76b968aa9682a84f530f6e9404f0a4af080f6b
amos <amos.doan@gmail.com>
2019-11-13 20:23:01 +0900

    Session #1 연습

---
+++ privateClass/listPractice2.py
@@ -0,0 +1,13 @@
+southKoreaCity = ["Seoul", "Daegu", "Daejeon"]
+northKoreaCity = ["평양", "원산", "개마고원"]
+
+koreCity = southKoreaCity + northKoreaCity
+print(koreCity)
+
+
+koreCity.remove("평양")
+print(koreCity)
+
+num = [1, 100, -10, -30, 20, 15]
+num.sort()
+print(num)
